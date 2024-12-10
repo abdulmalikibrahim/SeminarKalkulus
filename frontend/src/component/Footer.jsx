3140b756de2d4b510f94f35bcc6ab032c84ddc4a
Malik Thinkpad <cuymalik915@gmail.com>
2024-12-11 01:29:31 +0700

    update footer

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
                                         <img alt="Beefree Logo" height="32px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaSBcA59UZwqv5QitStUL0FiR7ckkHyL6Svw&s" width="auto" />
                                     </a>
                                 </div>
-                                <div className="bee-icon-label bee-icon-label-right">
+                                <div className="bee-icon-label bee-icon-label-right" style={{fontSize:"9pt"}}>
                                     <a href="https://www.linkedin.com/in/abdul-ibrahim-807748242?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" title="Designed with Beefree"> Created By : Abdul Malik Ibrahim (T1.24.B1) </a>
                                 </div>
                             </div>
