4eee875dda39c5c56ba936e7be5b481454e198b7
Malik Thinkpad <cuymalik915@gmail.com>
2024-12-21 22:46:24 +0700

    update kalkulus

--- frontend/src/pages/PeraturanSeminar.jsx
+++ frontend/src/pages/PeraturanSeminar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { saveAs } from 'file-saver'
 
 const PeraturanSeminar = () => {
     return(
@@ -30,14 +31,28 @@ const Main = ({children}) => {
 }
 
 const Peraturan = () => {
+    const downloadImage = () => {
+        const fileUrl = `${process.env.PUBLIC_URL}/images/Background Webinar.jpg`; // URL file
+        const fileName = "Background Webinar Kalkulus.jpg"; // Nama file yang akan diunduh
+        saveAs(fileUrl, fileName);
+    }
+    
     return(
-        <ul className='text-start'>
-            <li>Mohon sudah stand by di website ini setidak nya 10 menit sebelum di mulai</li>
-            <li>Tidak menggunakan link yang di share oleh siapapun karena saat anda mendapatkan link dari orang lain anda akan di anggap tidak hadir</li>
-            <li>Mohon untuk bisa <i className='fas fa-video'></i> ONCAM</li>
-            <li>Mohon menggunakan kata-kata yang sopan saat bertanya.</li>
-            <li>Anda bisa bertanya mengajukan pertanyaan saat pemateri berjalan melalui halaman <Link style={{textDecoration:"none"}} to="/linkzoom">Link Zoom</Link>, namun pertanyaan akan kami bacakan setelah pembahasan materi.</li>
-        </ul>
+        <div>
+            <ul className='text-start'>
+                <li>Mohon sudah stand by di website ini setidak nya 10 menit sebelum di mulai</li>
+                <li>Tidak menggunakan link yang di share oleh siapapun karena saat anda mendapatkan link dari orang lain anda akan di anggap tidak hadir</li>
+                <li>Mohon untuk bisa <i className='fas fa-video'></i> ONCAM</li>
+                <li>Mohon menggunakan kata-kata yang sopan saat bertanya.</li>
+                <li>Mohon untuk menggunakan link zoom di halaman <Link style={{textDecoration:"none"}} to="/linkzoom">Link Zoom</Link>, ketika anda klik link zoom maka anda akan dihitung hadir dan bisa mencetak sertifikat mandiri.</li>
+                <li>Download Background Webinar <a onClick={downloadImage} href="javascript:void(0)" style={{textDecoration:"none"}}>di sini</a></li>
+            </ul>
+            <div className='row d-flex justify-content-center'>
+                <div className="col-lg-8">
+                    <img src="/images/Background Webinar.jpg" alt='Background Webinar Kalkulus' width="100%"/>
+                </div>
+            </div>
+        </div>
     )
 }
 
